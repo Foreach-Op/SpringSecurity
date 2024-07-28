68aa8b1a59a2036ff01fc01df308d3249f7d8203
Foreach-Op <59776888+Foreach-Op@users.noreply.github.com>
2024-07-29 00:18:14 +0300

    Init

---
+++ springsecurityv1/src/main/java/com/foreach/springsecurityv1/Springsecurityv1Application.java
@@ -0,0 +1,13 @@
+package com.foreach.springsecurityv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecurityv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecurityv1Application.class, args);
+	}
+
+}
