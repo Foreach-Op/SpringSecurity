00909a759b4eaa94cc8b3f257fb22b389ec388d6
Foreach-Op <59776888+Foreach-Op@users.noreply.github.com>
2024-08-01 12:16:07 +0300

    Session 3

---
+++ springsecurityv3/src/main/java/com/foreach/controller/NoticesController.java
@@ -0,0 +1,13 @@
+package com.foreach.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticesController {
+
+    @GetMapping("/notices")
+    public String getNotices(){
+        return "Notices Details";
+    }
+}
